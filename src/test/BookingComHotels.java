ce9a4eab0002784daf41ec75d60026e510db8157
Alexander Trofimenko <oleksandr.trofimenko@amadeus.com>
2019-10-18 20:37:26 +0300

    second

--- src/test/BookingComHotels.java
+++ src/test/BookingComHotels.java
@@ -2,13 +2,17 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
-import test.Pages.*;
-//import test.Pages.SearchHotelPage;
+import test.Pages.SearchHotelPage;
+import test.Pages.SearchResultsHotelsPage;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+//import test.Pages.SearchHotelPage;
 
 public class BookingComHotels {
     WebDriver webDriver;
@@ -24,17 +28,29 @@ public class BookingComHotels {
     @Test
     public  void twoAdultSearchTest(){
         SearchHotelPage  searchHotel = new SearchHotelPage(webDriver);
+        int numberOfNight = 3;
+        int numberOfAdults = 2;
         searchHotel.putValueInsearchInputElementField("Прага");
         searchHotel.clickSelectSearchDirection();
         searchHotel.clickCheck_inDate();
         searchHotel.clickCheck_outDate();
         searchHotel.clickSearchOffersButton();
+        checkResult(numberOfNight, numberOfAdults);
 
 
     }
 
+    private void checkResult(int numberOfNight, int numberOfAdults) {
+        SearchResultsHotelsPage searchResultsHotelsPage = new SearchResultsHotelsPage(webDriver);
+        List<String> nightAndPeopleList =  searchResultsHotelsPage.getNumberOfPeopleAndNigntsString();
+        for (String str:nightAndPeopleList){
+            Assert.assertEquals(str,"Цена за " + numberOfNight + " ночи, " + numberOfAdults +" взрослых", "nightAndPeopleList");
+        }
+    }
+
+
     @AfterTest
     public void AfterSuite() {
-        webDriver.quit();
+     //   webDriver.quit();
     }
 }
--- src/test/Pages/SearchResultsHotelsPage.java
+++ src/test/Pages/SearchResultsHotelsPage.java
@@ -1,4 +1,40 @@
 package test.Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchResultsHotelsPage {
+
+    WebDriver webDriver;
+
+    @FindBy(xpath = ".//div[@id='search_results_table']")
+    public WebElement searchResultsTableElement;
+
+    @FindBy(xpath = ".//div[@class='sr_item  sr_item_new sr_item_default sr_property_block  sr_flex_layout                 ']")
+    public ArrayList<WebElement> hotelSearchResultsTableElement;
+
+//    @FindBy(xpath = ".//div[@id='search_results_table']")
+//    public WebElement searchResultsTableElement;
+
+    public List<String> getNumberOfPeopleAndNigntsString(){
+        List<String> strList = new ArrayList<>();
+        for(WebElement el: this.hotelSearchResultsTableElement){
+            String NumberOfPeapleAndNignts = el.findElement(By.xpath(".//*[@class='bui-price-display__label prco-inline-block-maker-helper']")).getText();
+            strList.add(NumberOfPeapleAndNignts);
+        }
+        //Assert.assertTrue(false,"Количество людей и ночей  не соответвует поиску - " + NumberOfPeapleAndNignts );
+        return strList;
+    }
+
+    public searchResultsHotelsPage(WebDriver webDriver) {
+        PageFactory.initElements(webDriver, this);
+        this.webDriver = webDriver;
+    }
+
 }
